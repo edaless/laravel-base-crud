d15740bc2152537b656705bd0af4829aa78c253a
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2023-02-04 16:50:21 +0100

    model Saint fatto

---
+++ database/migrations/2023_02_04_154133_create_saints_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saints', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 32);
+            $table->string('birth_place', 32);
+            $table->date('blessing_date');
+            $table->integer('miracles_number')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saints');
+    }
+};
