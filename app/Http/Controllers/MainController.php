7282c1fc2a8ec9d9c2a62e0f8bb793ff24fb7cfd
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2023-02-10 12:12:01 +0100

    ogni santo stampato con dettagli

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Saint;
+
 class MainController extends Controller
 {
     public function home()
     {
-        return view('pages.home');
+        $saints = Saint::all();
+        $data = [
+            'saints' => $saints
+        ];
+        return view('pages.home', $data);
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,4 +3,17 @@
     
     <h1>Home blade</h1>
 
+    <ul>
+        @foreach ($saints as $saint)
+            <li>
+                <b>
+                    {{$saint->name}} <br>
+                </b>
+                {{$saint->birth_place}} <br>
+                {{$saint->blessing_date}} <br>
+                {{$saint->miracles_number}} <br>
+            </li>            
+        @endforeach
+    </ul>
+
 @endsection
